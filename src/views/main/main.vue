91738d64956ed5a6bc111d97bc04fef7c8432092
wujiamian <wujiamian@xgd.com>
2022-08-19 13:58:45 +0800

    feat(响应数据 reative toRefs ref) css 可以绑定变量

--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -1,4 +1,40 @@
 <template>
   <div>侧边栏</div>
+  <div @click="changName">changeName</div>
+  <div @click="changeAge">changeAge</div>
+  <div>{{ age }}{{ name }}</div>
+  <h3 @click="color = 'blue'">样式改变</h3>
   <router-view></router-view>
 </template>
+
+<script lang="ts">
+import { defineComponent, reactive, ref, toRefs } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const color = ref('red')
+    const data = reactive({
+      age: 10,
+      name: 'wujiam',
+      changName: function () {
+        this.name = 'zhangsan'
+      }
+    })
+    const changeAge = () => {
+      data.age = 13
+    }
+
+    return {
+      ...toRefs(data),
+      changeAge,
+      color
+    }
+  }
+})
+</script>
+
+<style scoped lang="scss">
+h3 {
+  color: v-bind(color);
+}
+</style>
